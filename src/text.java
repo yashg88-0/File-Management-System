bffa989a5663cfc17a7f6d0e81f9f3f3329416cd
tangent-cmd <65667041+tangent-cmd@users.noreply.github.com>
2021-04-08 11:37:11 +0530

    Update text.java

--- src/text.java
+++ src/text.java
@@ -2,15 +2,17 @@ import java.awt.event.*;
 import java.util.Random;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 class text extends JFrame implements ActionListener {
     static JTextField t1;
     static JTextField t2;
     static JFrame f;
-    static JButton b;
+    static JButton b, b1;
     static JLabel l1;
     static JLabel l2;
     text()
     {
+    	
     }
     public static void main(String[] args)
     {
@@ -18,8 +20,11 @@ class text extends JFrame implements ActionListener {
         l1 = new JLabel("Enter the name");
         l2 = new JLabel("Enter the age");
         
-        b = new JButton("submit");
-        b.addActionListener(new text()); //add action to button
+        b = new JButton("Submit");
+        b1 = new JButton("Clear");
+         //add action to button
+
+//        t2.addKeyListener(new text());
   
         text te = new text();
   
@@ -33,18 +38,59 @@ class text extends JFrame implements ActionListener {
         p.add(l2);
         p.add(t2);
         p.add(b);
+        p.add(b1);
        
         f.add(p);
         f.setSize(300, 300);
         f.show();
+        
+        b.addActionListener(new text());
+//        b1.addActionListener(new text());
+        t2.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+		           String value = t2.getText();
+//		           int l = value.length();
+		           if (e.getKeyChar() >= '0' && e.getKeyChar() <= '9'|| e.getKeyChar()==(char)8) {
+		              t2.setEditable(true);
+//		              l2.setText("");
+		           } else {
+		              t2.setEditable(false);
+		              l2.setText("* Enter only numeric digits(0-9)");
+		           }
+			}
+		}); 
+        b1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+				t1.setText("");
+				t2.setText("");
+                //textfield.setText(null); //or use this
+            }
+        });
+        
     }
-  
     public void actionPerformed(ActionEvent e)
     {
+    	String textFieldValue = t1.getText();
         	Random r = new Random();
         	int upperBound = 100;
         	int random = r.nextInt(upperBound);
             f=new JFrame();  
-            JOptionPane.showMessageDialog(f,"The Random number is \n"+String.valueOf(random));
+			JOptionPane.showMessageDialog(f,"The chances of winning of "+textFieldValue+" is "+String.valueOf(random)+" %");
     }
+
 }
